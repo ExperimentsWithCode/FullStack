18e18d9aae1b3e2e0ca24103ff820af593962e3b
Meir Galimidi <Meir@Meirs-MacBook-Pro-3.local>
2017-02-17 14:27:10 -0500

    Timestamps now accurate. Previously defaulted to time of migration which created those columns.

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217005542) do
+ActiveRecord::Schema.define(version: 20170217192221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title",                                      null: false
-    t.text     "body",                                       null: false
-    t.integer  "author_id",                                  null: false
-    t.datetime "created_at", default: '2017-02-17 01:10:55', null: false
-    t.datetime "updated_at", default: '2017-02-17 01:10:55', null: false
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_questions_on_author_id", using: :btree
     t.index ["title"], name: "index_questions_on_title", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",                                        null: false
-    t.string   "email",                                           null: false
-    t.string   "password_digest",                                 null: false
-    t.string   "session_token",                                   null: false
-    t.datetime "created_at",      default: '2017-02-17 01:10:55', null: false
-    t.datetime "updated_at",      default: '2017-02-17 01:10:55', null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["session_token"], name: "index_users_on_session_token", using: :btree
     t.index ["username"], name: "index_users_on_username", using: :btree
