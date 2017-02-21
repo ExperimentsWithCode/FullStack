64d03fd6aaf397a6c94939dee2707b3414e9bafa
Meir Galimidi <MeirWorks@gmail.com>
2017-02-21 16:25:48 -0500

    Functional Show page. Needs answers to be built in.

--- app/views/api/questions/_question.json.jbuilder
+++ app/views/api/questions/_question.json.jbuilder
@@ -1,1 +1,4 @@
 json.extract! question, :id, :title, :body, :author_id, :created_at, :updated_at
+json.author do
+  json.extract! question.author, :id, :username
+end
--- frontend/components/current_question/current_question_display.jsx
+++ frontend/components/current_question/current_question_display.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import { SideNavDisplay } from '../nav/side_nav_display';
+import Moment from 'moment'
 
 class CurrentQuestionDisplay extends React.Component {
 	constructor(props) {
@@ -15,7 +16,6 @@ class CurrentQuestionDisplay extends React.Component {
 
 
 	componentWillReceiveProps(newProps){
-    debugger
 		this.state = newProps.question;
 	}
 
@@ -25,17 +25,69 @@ class CurrentQuestionDisplay extends React.Component {
 	}
 
 
+  formatDate(theDate){
+    return `asked ${Moment(theDate).format('MMM Do YYYY, h:mm:ss a')}`;
+  }
 
+	renderQuestion() {
+		if (this.state) {
+			if (this.state.currentQuestion.id !== undefined ) {
+        debugger
+				return (
+          <div className="question-display">
+            <div className="question-display-content">
+              <div className="question-stat-bar">
+                <span className="list-view-score">0</span>
+                <span className="list-view-label">answers</span>
+              </div>
+              <p>{this.state.currentQuestion.body}</p>
+            </div>
+            <div className="question-display-content">
+              <div className="question-stat-bar">
+              </div>
+              <div className="question-display-footer">
+                <Link to={`/ask/${this.props.params.id}`}>Edit</Link>
+                <div className="author-info">
+                  <p>{this.formatDate(this.state.currentQuestion.created_at)}</p>
+                  <p>{this.state.currentQuestion.author.username}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+			);
+			}
+		}
+		return (<p> Nothing to render </p>)
+	}
+  renderQuestionHeader() {
+    if (this.state) {
+			if (this.state.currentQuestion !== undefined ) {
+
+				return (
+          <div className="question-header">
+            <div className="sub-header-content full">
+              <h2>{this.state.currentQuestion.title}</h2>
+              <Link to="/ask" className='submit'>Ask Question</Link>
+            </div>
+          </div>
+			);
+			}
+		}
+		return (<p> Nothing to render </p>)
+	}
 
 	render() {
 		return (
       <div className="container">
+        {this.renderQuestionHeader()}
   			<div className="split-content">
 					<div className="main-content">
-            <h1>Main Content</h1>
+            <div className="questions-list">
+              {this.renderQuestion()}
+            </div>
 					</div>
 					<div className="side-content">
-            <SideNavDisplay questions={null} />
 					</div>
         </div>
       </div>
@@ -49,3 +101,28 @@ export default withRouter(CurrentQuestionDisplay);
 
 // {location.hash.slice(2).includes("") ? "All Questions" : "Unanswered"}
 //
+
+// BOTTOM ROW ON QUESTION PAGE
+// <table class="fw">
+//     <tbody><tr>
+//     <td class="vt">
+// <div class="post-menu"><a href="/q/42375655/1810266" title="short permalink to this question" class="short-link" id="link-post-42375655">share</a><span class="lsep">|</span><span class="disabled-link" title="Another edit is awaiting approval for this post. Further edits cannot be submitted until the pending edit is reviewed.">edit</span><span class="lsep">|</span><a href="#" class="flag-post-link" title="flag this post for serious problems or moderator attention" data-postid="42375655">flag</a></div>
+//     </td>
+//     <td class="post-signature owner">
+//         <div class="user-info ">
+//     <div class="user-action-time">
+//         asked <span title="2017-02-21 18:53:53Z" class="relativetime">1 hour ago</span>
+//     </div>
+//     <div class="user-gravatar32">
+//         <a href="/users/5589727/phil-viton"><div class="gravatar-wrapper-32"><img src="https://www.gravatar.com/avatar/dc55e569252429a92fb532e11cbd081d?s=32&amp;d=identicon&amp;r=PG&amp;f=1" alt="" width="32" height="32"></div></a>
+//     </div>
+//     <div class="user-details">
+//         <a href="/users/5589727/phil-viton">Phil Viton</a>
+//         <div class="-flair">
+//             <span class="reputation-score" title="reputation score " dir="ltr">1</span>
+//         </div>
+//     </div>
+// </div>
+//     </td>
+//     </tr>
+//     </tbody></table>
