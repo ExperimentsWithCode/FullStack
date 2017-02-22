6190ed3e755e3fa738e15b9e3ad406d9640e9080
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-02-22 17:37:03 -0500

    Render Answer form on Question Show page

--- frontend/components/answer_form/answer_form_display.jsx
+++ frontend/components/answer_form/answer_form_display.jsx
@@ -4,75 +4,68 @@ import { Link, withRouter } from 'react-router';
 class AnswerFormDisplay extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { title: "", body: "", author_id: this.props.current_user.id };
+		this.state = { body: "", author_id: this.props.current_user.id, question_id: this.props.params.id};
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-// 
-// 	componentWillReceiveProps(newProps){
-// 		if (newProps.route === this.props.route){
-// 			this.state = { title: "", body: "", author_id: this.props.current_user.id };
-// 		}
-// 	}
-//
-// 	componentDidUpdate() {
-// 		this.ensureLoggedIn();
-// 	}
-//
-// 	ensureLoggedIn() {
-// 		if (!this.props.loggedIn) {
-// 			this.props.router.push("/login");
-// 		}
-// 	}
-//
-// 	update(field) {
-// 		return e => this.setState({
-// 			[field]: e.currentTarget.value
-// 		});
-// 	}
-//
-// 	handleSubmit(e) {
-// 		e.preventDefault();
-// 		const currentQuestion = this.state;
-// 		this.props.create(currentQuestion);
-// 	}
-//
-// 	renderErrors() {
-// 		if (this.props.errors.length > 0){
-// 		return(
-// 			<ul className="errors" >
-// 				{this.props.errors.map((error, i) => (
-// 					<li key={`error-${i}`}>
-// 						{error}
-// 					</li>
-// 				))}
-// 			</ul>
-// 		)};
-// 	};
-//
-// 	render() {
-// 		return (
-//       <div className="container">
-// 				<div className="split-content">
-// 	        <form onSubmit={this.handleSubmit} className="main-content">
-// 						{this.renderErrors()}
-// 						<div className="question-title-input" >
-// 							<label> Title</label>
-// 							<input type="text"
-// 								value={this.state.title}
-// 								onChange={this.update("title")} />
-// 						</div>
-//
-// 						<div className="question-body-input" >
-// 							<textarea className="question-body-textarea" rows="10" onChange={this.update("body")} value={this.state.body}>
-// 							</textarea>
-// 						</div>
-// 						<input type="submit" value="Post Your Question" className="submit question"/>
-// 	        </form>
-// 	      </div>
-//       </div>
-// 		);
-// 	}
-//
-// }
+
+	componentWillReceiveProps(newProps){
+		if (newProps.route === this.props.route){
+			this.state = { body: "", author_id: this.props.current_user.id, question_id: this.props.params.id};
+		}
+	}
+
+	// componentDidUpdate() {
+	// 	this.ensureLoggedIn();
+	// }
+
+	ensureLoggedIn() {
+		if (!this.props.loggedIn) {
+			this.props.router.push("/login");
+		}
+	}
+
+	update(field) {
+		return e => this.setState({
+			[field]: e.currentTarget.value
+		});
+	}
+
+	handleSubmit(e) {
+      this.ensureLoggedIn();
+		e.preventDefault();
+		const currentQuestion = this.state;
+		this.props.create(this.state);
+	}
+
+	renderErrors() {
+		if (this.props.errors.length > 0){
+		return(
+			<ul className="errors" >
+				{this.props.errors.map((error, i) => (
+					<li key={`error-${i}`}>
+						{error}
+					</li>
+				))}
+			</ul>
+		);}
+	}
+
+	render() {
+    debugger
+		return (
+      <form onSubmit={this.handleSubmit} className="main-content">
+        <h3>Your Answer</h3>
+
+        {this.renderErrors()}
+        <div className="question-body-input" >
+          <textarea className="question-body-textarea" rows="10" onChange={this.update("body")} value={this.state.body}>
+          </textarea>
+        </div>
+        <input type="submit" value="Post Your Question" className="submit question"/>
+      </form>
+    );
+ 	}
+
+}
 
 export default withRouter(AnswerFormDisplay);
