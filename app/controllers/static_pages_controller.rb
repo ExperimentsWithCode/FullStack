e48fd79fab137d4033c703ec5597e54c646aefba
Meir Galimidi <Meir@Meirs-MacBook-Pro-3.local>
2017-03-15 16:22:06 -0400

    Add SSL through letsencrypt

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def root
     render :root
   end
+  
+  def letsencrypt
+    render text: "V3MmfkQRvlYVXf5WK6xev3TmSJJp4hl1jdXMIXKUZTU.uxS5mcKehnPLRvXiR0yvDk_P1dCN8eQyEboui1W4hGE"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,24 @@ Rails.application.routes.draw do
   end
 
   root "static_pages#root"
+  get '/.well-known/acme-challenge/:id' => '"static_pages#letsencrypt'
+
 end
+
+
+# Make sure your web server displays the following content at
+# http://heapspill.com/.well-known/acme-challenge/V3MmfkQRvlYVXf5WK6xev3TmSJJp4hl1jdXMIXKUZTU before continuing:
+#
+# V3MmfkQRvlYVXf5WK6xev3TmSJJp4hl1jdXMIXKUZTU.uxS5mcKehnPLRvXiR0yvDk_P1dCN8eQyEboui1W4hGE
+#
+# If you don't have HTTP server configured, you can run the following
+# command on the target server (as root):
+#
+# mkdir -p /tmp/certbot/public_html/.well-known/acme-challenge
+# cd /tmp/certbot/public_html
+# printf "%s" V3MmfkQRvlYVXf5WK6xev3TmSJJp4hl1jdXMIXKUZTU.uxS5mcKehnPLRvXiR0yvDk_P1dCN8eQyEboui1W4hGE > .well-known/acme-challenge/V3MmfkQRvlYVXf5WK6xev3TmSJJp4hl1jdXMIXKUZTU
+# # run only once per server:
+# $(command -v python2 || command -v python2.7 || command -v python2.6) -c \
+# "import BaseHTTPServer, SimpleHTTPServer; \
+# s = BaseHTTPServer.HTTPServer(('', 80), SimpleHTTPServer.SimpleHTTPRequestHandler); \
+# s.serve_forever()"
